ad4d4f33a48e055f047370e8b9fbe73e4bc57011
JP Talusan <jptalusan@gmail.com>
2024-04-25 00:51:18 -0500

    Are -fPIC and set_target_properties redundant? Added O2 for optimization

--- python/run.py
+++ python/run.py
@@ -14,6 +14,7 @@ plt.plot(Simulation.v_data, Simulation.v_gamma, "--", linewidth=3, color=(1, 0, 
 a = pd.DataFrame(Simulation.v_data)
 print(a)
 plt.ylim(-20, 10)
+plt.xlim(-20, 10)
 plt.xlabel("x")
 plt.ylabel("($f(x) = \gamma(x)$)")
 plt.title("(Gamma Function: $\gamma(z) = \int_0^\infty x^{z-1} e^{-x} dx$)", fontsize=18)
